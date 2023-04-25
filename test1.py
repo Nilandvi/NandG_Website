6fdc3955381f3a9cf1f026d9a40739adb8ac095a
Nilandvi <Nilandvi@gmail.com>
2023-04-26 00:34:36 +0300

    test form reg

---
+++ test1.py
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../static/img/logo.png">
+    <link rel="stylesheet" href="../static/css/style.css">
+    <title>N&G Website</title>
+  </head>
+  <body>
+    <form class="login_form" method="post">
+        <body style="background-color: #101415;">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+            <header>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label white">NickName</label>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">@</span>
+                        <input type="text" class="form-control" name="username" placeholder="Короткое имя" aria-label="Username" aria-describedby="basic-addon1">
+                      </div>
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label white">Пароль</label>
+                    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Продолжить</button>
+            </header>
+      </form>
+  </body>
+</html>
