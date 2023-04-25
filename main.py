dc56c051329d69e95ebdec080d7b4e18856cd4c9
Nicolay237 <iordanovn21@gmail.com>
2023-04-26 00:27:21 +0300

    уххххххх

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from data import db_session
 from data.users import User
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
@@ -20,12 +21,14 @@ def index():
 @app.route('/')
 @app.route('/autorization', methods=['POST', 'GET'])
 def autorization_form_sample():
+    db_session.global_init("db/blogs.db")
+    session = db_session.create_session()
     if request.method == 'GET':
       return render_template("autorization.html", title="Jinja and Flask")
     elif request.method == 'POST':
         print(request.form['username'])
         print(request.form['password'])
-        return "Форма отправлена"
+        return redirect(url_for('account', number=3))
 
 @app.route('/')
 @app.route('/registration', methods=['POST', 'GET'])
@@ -34,8 +37,16 @@ def registration():
       return render_template("registration.html", title="Jinja and Flask")
     elif request.method == 'POST':
         register(request.form['nickname'], request.form['name'], request.form['about'], request.form['mail'], request.form['password'])
-        return "Форма отправлена и сохранена в базу данных"
+        print(request.form['nickname'], request.form['name'], request.form['about'], request.form['mail'], request.form['password'])
+        return redirect((url_for('autorization_form_sample')))
 
+
+@app.route('/account/<number>')
+def account(number):
+    db_session.global_init("db/blogs.db")
+    session = db_session.create_session()
+    user = session.query(User).filter(User.id == int(number)).first()
+    return render_template('account.html', title='Account', user=user)
 
 
 if __name__ == '__main__':
