51bd9d7da42e4bdaa908db73953f683bfa934690
Nilandvi <Nilandvi@gmail.com>
2023-04-26 20:23:46 +0300

    Rollback

--- data/news.py
+++ data/news.py
@@ -5,8 +5,8 @@ from sqlalchemy import orm
 from .db_session import SqlAlchemyBase
 
 
-class Note(SqlAlchemyBase):
-    __tablename__ = 'notes'
+class News(SqlAlchemyBase):
+    __tablename__ = 'news'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
@@ -14,7 +14,8 @@ class Note(SqlAlchemyBase):
     content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
-    deathline = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     user = orm.relationship('User')
--- main.py
+++ main.py
@@ -1,43 +1,15 @@
 from flask import Flask, render_template, request, url_for, redirect
 from data import db_session
 from data.users import User
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, DateField, IntegerField
-from wtforms.validators import DataRequired, Length, Optional, NumberRange
-from data.news import Note
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
-
-class NoteForm(FlaskForm):
-    title = StringField('Заголовок', validators=[DataRequired(), Length(max=255)])
-    content = TextAreaField('Контент', validators=[DataRequired()])
-    deathline = StringField('Дедлайн', validators=[Optional()])
 
 def register(nickname, name, about, mail, password):
     db_session.global_init("db/blogs.db")
     session = db_session.create_session()
     user = User(nickname=nickname, name=name, about=about, email=mail, hashed_password=password)
     session.add(user)
-    session.commit()
-
-def note(title, content, deathline):
-    db_session.global_init("db/blogs.db")
-    session = db_session.create_session()
-    note = Note(title=title, content=content, deathline=deathline)
-    session.add(note)
     session.commit()
 
 
@@ -66,8 +38,8 @@ def registration():
     elif request.method == 'POST':
         register(request.form['nickname'], request.form['name'], request.form['about'], request.form['mail'], request.form['password'])
         print(request.form['nickname'], request.form['name'], request.form['about'], request.form['mail'], request.form['password'])
-        print(request.form['nickname'], request.form['name'], request.form['about'], request.form['mail'], request.form['password'])
         return redirect((url_for('autorization_form_sample')))
+
 
 @app.route('/account/<number>')
 def account(number):
@@ -75,15 +47,6 @@ def account(number):
     session = db_session.create_session()
     user = session.query(User).filter(User.id == int(number)).first()
     return render_template('account.html', title='Account', user=user)
-
-@app.route('/')
-@app.route('/note', methods=['POST', 'GET'])
-def notecreate():
-    if request.method == 'GET':
-      return render_template("note.html", title="Jinja and Flask")
-    elif request.method == 'POST':
-        register(request.form['title'], request.form['content'], request.form['deathline'])
-        return "Форма отправлена и сохранена в базу данных"
 
 
 if __name__ == '__main__':
